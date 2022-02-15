dfc78dc948868faf45d10d4aa939556b4c716b6d
TanishqAhluwalia25 <tanishqwalia25@gmail.com>
2022-02-15 11:21:33 +0530

    Added more videos

--- components/ReelsPage.js
+++ components/ReelsPage.js
@@ -12,9 +12,12 @@ import ClientOnly from "../components/ClientOnly";
 
 const videos = [
   "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-  "https://d33tbglx0zf09c.cloudfront.net/5e803630-4888-4a2d-a163-b28135644214-1644658890.5965555converted.mp4",
-  "https://livesim.dashif.org/livesim/chunkdur_1/ato_7/testpic4_8s/Manifest.mpd",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd",
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+  "https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8",
+  "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+  // "https://d33tbglx0zf09c.cloudfront.net/5e803630-4888-4a2d-a163-b28135644214-1644658890.5965555converted.mp4",
+  // "https://livesim.dashif.org/livesim/chunkdur_1/ato_7/testpic4_8s/Manifest.mpd",
+  // "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd",
   // "https://dash.akamaized.net/dash264/TestCasesUHD/2b/11/MultiRate.mpd",
 ];
 
